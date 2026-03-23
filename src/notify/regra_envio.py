2bd9b922be9deba04f8244d08eb412baa6537245
Nicolas-Cavalcante <nicolas.cavalcante@flytour.com.br>
2026-03-23 18:09:08 -0300

    feat: Alterações feitas:  Arquivo .env criado, apenas atribuindo login SQL.Ajuste na exportação dos arquivos, validando arquivo de log + arquivo com casos corretos e incorretos.

--- src/notify/regra_envio.py
+++ src/notify/regra_envio.py
@@ -1,21 +1,40 @@
 from datetime import datetime
 
-def definir_destinatarios(dias_para_corte, config):
+def definir_destinatarios(dias_para_corte, df_base_email):
+
+    df_base_email['E-mail Célula'] = df_base_email['E-mail Célula'].astype(str).str.lower().str.strip()
+
+    # separa grupos
+    operacao = (
+    df_base_email['E-mail Célula']
+    .dropna()
+    .astype(str)
+    .str.lower()
+    .str.strip()
+)
+
+    operacao = operacao[operacao != "nan"].tolist()
+    #gestao = df_base_email[df_base_email['tipo'] == 'gestao']['email'].dropna().tolist()
+    #diretoria = df_base_email[df_base_email['tipo'] == 'diretoria']['email'].dropna().tolist()
 
     #Busca dia e semana atual do processamento
     hoje = datetime.today()
     dia_semana = hoje.weekday() # 0=segunda, 6=domingo
 
-    #Ajuste em casos de fim de semana
+    # REGRA DE FIM DE SEMANA
     if dias_para_corte == -4 and dia_semana >= 5:
-        dias_para_corte = -4 # mas deveria ter sido tratado antes (sexta)
+        print("⚠️ Hoje é fim de semana e deveria ter sido enviado na sexta")
 
-    # Regras
-    if dias_para_corte <= 0:
-        return config.EMAIL_OPERACAO + config.EMAIL_GESTAO + config.EMAIL_DIRETORIA
+        # Aqui você decide:
+        # opção 1 → NÃO envia
+        return []
 
-    elif dias_para_corte <= config.DIAS_ALERTA:
-        return config.EMAIL_OPERACAO + config.EMAIL_GESTAO
+        # opção 2 → força envio
+        # return list(set(operacao + gestao + diretoria))
+
+    # regras normais
+    if dias_para_corte <= 5:
+        return list(set(operacao))
 
     else:
-        return config.EMAIL_OPERACAO
+        return operacao
