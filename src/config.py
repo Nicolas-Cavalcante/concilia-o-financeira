992b23cbfb07a70a103fc4ae2c2c40b88cb84ad6
Nicolas-Cavalcante <nicolas.cavalcante@flytour.com.br>
2026-03-23 18:09:32 -0300

    feat: adiciona configuração via .env e estrutura inicial de envio de email. Houve falha no processamento, pois a maquina local esta com mais de um outlook instalado.
    - remove credenciais hardcoded;
    - adiciona leitura de variáveis de ambiente;
    - implementa base inicial de envio de email com anexo.
    
    This reverts commit 6c94dfd17975c184d13696789115a392dcd89453.

--- src/config.py
+++ src/config.py
@@ -1,9 +1,11 @@
 INPUT_PATH = r"C:\Users\nicolas.cavalcante\OneDrive - BEFLY TRAVEL\Área de Trabalho\Teste_python\EBTA\PendenciasAereo_20260305-092004.xlsx"
-INPUT_PATH2 = r"C:\Users\nicolas.cavalcante\OneDrive - BEFLY TRAVEL\Área de Trabalho\Teste_python\EBTA\25FEB26_Contatos Squads_SAO e CPQ.xlsx"
-
 
 OUTPUT_PATH = "outputs/"
 LOG_PATH = "logs/historico_execucao.xlsx"
+
+EMAIL_OPERACAO = ["op@empresa.com"]
+EMAIL_GESTAO = ["gestao@empresa.com"]
+EMAIL_DIRETORIA = ["diretoria@empresa.com"]
 
 DIAS_ALERTA = 5
 DIAS_CRITICO = 2
--- src/notify/regra_envio.py
+++ src/notify/regra_envio.py
@@ -1,40 +1,21 @@
 from datetime import datetime
 
-def definir_destinatarios(dias_para_corte, df_base_email):
-
-    df_base_email['E-mail Célula'] = df_base_email['E-mail Célula'].astype(str).str.lower().str.strip()
-
-    # separa grupos
-    operacao = (
-    df_base_email['E-mail Célula']
-    .dropna()
-    .astype(str)
-    .str.lower()
-    .str.strip()
-)
-
-    operacao = operacao[operacao != "nan"].tolist()
-    #gestao = df_base_email[df_base_email['tipo'] == 'gestao']['email'].dropna().tolist()
-    #diretoria = df_base_email[df_base_email['tipo'] == 'diretoria']['email'].dropna().tolist()
+def definir_destinatarios(dias_para_corte, config):
 
     #Busca dia e semana atual do processamento
     hoje = datetime.today()
     dia_semana = hoje.weekday() # 0=segunda, 6=domingo
 
-    # REGRA DE FIM DE SEMANA
+    #Ajuste em casos de fim de semana
     if dias_para_corte == -4 and dia_semana >= 5:
-        print("⚠️ Hoje é fim de semana e deveria ter sido enviado na sexta")
+        dias_para_corte = -4 # mas deveria ter sido tratado antes (sexta)
 
-        # Aqui você decide:
-        # opção 1 → NÃO envia
-        return []
+    # Regras
+    if dias_para_corte <= 0:
+        return config.EMAIL_OPERACAO + config.EMAIL_GESTAO + config.EMAIL_DIRETORIA
 
-        # opção 2 → força envio
-        # return list(set(operacao + gestao + diretoria))
-
-    # regras normais
-    if dias_para_corte <= 5:
-        return list(set(operacao))
+    elif dias_para_corte <= config.DIAS_ALERTA:
+        return config.EMAIL_OPERACAO + config.EMAIL_GESTAO
 
     else:
-        return operacao
+        return config.EMAIL_OPERACAO
