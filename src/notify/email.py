d6e5549e21a86b3dbebe1afbcc4906441ab81d04
Nicolas-Cavalcante <nicolas.cavalcante@flytour.com.br>
2026-03-24 18:01:52 -0300

    feat: Alterações feitas:  Correção na biblioteca win32com, inclusao do arquivo processamento.py, ajuste nos demais itens de processamento
    - Alterei mail._oleobj_.Invoke(*(64209, 0, 8, 0, conta_encontrada)) para mail.SentOnBehalfOfName. O Invoke busca o email logado no outlook e não responde alias, já o segungo parametro aceita alias e não necessita de login em tempo real ;
    - Arquivo processamento.py cria as duas funções para o executavel, com parte de pergunta ao usuario sobre o envio do email e para criar uma barra de processamento apresentavel ao usuario;
    - o.

--- src/notify/email.py
+++ src/notify/email.py
@@ -4,28 +4,12 @@ import win32com.client as win32
 def enviar_email(email_origem,destinatarios, assunto, corpo, anexos=None):
     outlook = win32.Dispatch("Outlook.Application")
     mail = outlook.CreateItem(0)
-    conta_encontrada = None
     
-        # Seleciona a conta correta
-    
-    email_origem = email_origem.strip().lower()
-
-    for conta in outlook.Session.Accounts:
-        smtp = conta.SmtpAddress.strip().lower()
-    
-        if smtp == email_origem:
-            conta_encontrada = conta
-            break
-
-    if not conta_encontrada:
-        raise ValueError(f"Conta {email_origem} não encontrada no Outlook")
-    else:
-        # força uso da conta
-        mail._oleobj_.Invoke(*(64209, 0, 8, 0, conta_encontrada))
-
     mail.To = ";".join(destinatarios)
     mail.Subject = assunto
     mail.HTMLBody = corpo
+
+    mail.SentOnBehalfOfName = email_origem
 
     if anexos:
         for caminho in anexos:
