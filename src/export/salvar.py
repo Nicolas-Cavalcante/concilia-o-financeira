31e6d765f10d1d405aee658a57df2439038b1346
Nicolas-Cavalcante <nicolas.cavalcante@flytour.com.br>
2026-03-25 15:32:51 -0300

    feat: Alterações feitas:  Criação da interface com os caminhos dinamicos

--- src/export/salvar.py
+++ src/export/salvar.py
@@ -1,9 +1,6 @@
 import os
 
-def salvar(df_final, df_nao_localizados, base_path):
+def salvar(df_final, df_nao_localizados, path_corretos, path_incorretos):
 
-    os.makedirs(f"{base_path}/Corretos", exist_ok=True)
-    os.makedirs(f"{base_path}/Incorretos", exist_ok=True)
-
-    df_final.to_excel(f"{base_path}/Corretos/preenchidos.xlsx", index=False)
-    df_nao_localizados.to_excel(f"{base_path}/Incorretos/Pendências_EBTA.xlsx", index=False)
+    df_final.to_excel(path_corretos / "Corretos.xlsx", index=False)
+    df_nao_localizados.to_excel(path_incorretos / "Incorretos.xlsx", index=False)
---
+++ src/interface.py
@@ -0,0 +1,83 @@
+import tkinter as tk
+from tkinter import ttk
+import threading
+from tkinter import messagebox
+from tkinter import filedialog
+from src.main import main
+
+
+# ==============================
+# ROOT (uma única instância)
+# ==============================
+root = tk.Tk()
+root.withdraw()
+
+
+#==============================================
+#🔧 1. SELEÇÃO DE ARQUIVOS
+#==============================================
+def selecionar_arquivo(titulo):
+    return filedialog.askopenfilename(
+        title=titulo,
+        filetypes=[("Excel", "*.xlsx")]
+    )
+
+def iniciar_processo():
+    input1 = selecionar_arquivo("Selecione o arquivo de pendências")
+    input2 = selecionar_arquivo("Selecione a base de clientes")
+
+    if not input1 or not input2:
+        messagebox.showerror("Erro", "Selecione ambos os arquivos")
+        return
+
+    enviar_email = perguntar_envio_email()
+    tela_processamento(lambda: main(input1, input2, enviar_email))
+
+#==============================================
+#🔧 2. CONFIRMAÇÃO DE ENVIO DO E-MAIL
+#==============================================
+
+def perguntar_envio_email():
+    return messagebox.askyesno(
+        "Envio de Email",
+        "Deseja enviar o email ao final do processamento?"
+    )
+
+
+#==============================================
+#🔧 3. POPUP DE PROCESSAMENTO DO CÓDIGO
+#==============================================
+
+def tela_processamento(funcao_processamento):
+    root.deiconify()
+    root.title("Processamento Pendências EBTA")
+    root.geometry("280x200")
+
+    label = tk.Label(root, text="Iniciando...", font=("Arial", 12))
+    label.pack(padx=50, pady=30)
+
+    progress = ttk.Progressbar(root, mode="indeterminate")
+    progress.pack(padx=20, pady=10)
+    progress.start()
+
+    def rodar():
+        try:
+            root.after(0, lambda: label.config(text="Processando dados..."))
+            funcao_processamento()
+
+            root.after(0, lambda: label.config(text="Concluído com sucesso!"))
+            root.after(2000, root.destroy)
+
+        except Exception as e:
+            root.after(0, lambda: label.config(text=f"Erro: {e}"))
+
+        root.after(0, progress.stop)
+
+    threading.Thread(target=rodar, daemon=True).start()
+    root.mainloop()
+
+# ==============================
+# 4. EXECUÇÃO
+# ==============================
+if __name__ == "__main__":
+    iniciar_processo()
