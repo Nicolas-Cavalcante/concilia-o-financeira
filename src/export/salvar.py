3c2b105c0dd9ed7b4fc7804a6dba99fed270fcb2
Nicolas-Cavalcante <nicolas.cavalcante@flytour.com.br>
2026-03-26 18:22:16 -0300

    Feat: Versão Correta

--- src/export/salvar.py
+++ src/export/salvar.py
@@ -3,4 +3,4 @@ import os
 def salvar(df_final, df_nao_localizados, path_corretos, path_incorretos):
 
     df_final.to_excel(path_corretos / "Corretos.xlsx", index=False)
-    df_nao_localizados.to_excel(path_incorretos / "Incorretos.xlsx", index=False)
+    df_nao_localizados.to_excel(path_incorretos / "Pendências_EBTA.xlsx", index=False)
