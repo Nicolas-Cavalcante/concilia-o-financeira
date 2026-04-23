ed80cb4664e948a6d3a6c67ad31ab39de56026fa
Nicolas-Cavalcante <nicolas.cavalcante@flytour.com.br>
2026-04-23 18:17:33 -0300

    Atualização

--- src/matching/conciliacao.py
+++ src/matching/conciliacao.py
@@ -13,22 +13,22 @@ def executar_matching(df_planilha, df_sql, df_depara):
     unicos_plan_k1 = df_planilha['Chave Aut + Data + Valor'][~df_planilha['Chave Aut + Data + Valor'].duplicated(keep=False)]
     chaves_validas_k1 = set(unicos_sql_k1).intersection(set(unicos_plan_k1))
 
-    # Chave KEY 3 🗝️
+    # Chave KEY 2 🗝️
     unicos_sql_k2 = df_sql['Chave Loc Cia + Data + Valor'][~df_sql['Chave Loc Cia + Data + Valor'].duplicated(keep=False)]
     unicos_plan_k2 = df_planilha['Chave Loc Cia + Data + Valor'][~df_planilha['Chave Loc Cia + Data + Valor'].duplicated(keep=False)]
     chaves_validas_k2 = set(unicos_sql_k2).intersection(set(unicos_plan_k2))
 
     # Chave KEY 4 🗝️
-    unicos_sql_k3 = df_sql['Chave Cartão + Data + Valor'][~df_sql['Chave Cartão + Data + Valor'].duplicated(keep=False)]
-    unicos_plan_k3 = df_planilha['Chave Cartão + Data + Valor'][~df_planilha['Chave Cartão + Data + Valor'].duplicated(keep=False)]
-    chaves_validas_k3 = set(unicos_sql_k3).intersection(set(unicos_plan_k3))
+    #unicos_sql_k3 = df_sql['Chave Cartão + Data + Valor'][~df_sql['Chave Cartão + Data + Valor'].duplicated(keep=False)]
+    #unicos_plan_k3 = df_planilha['Chave Cartão + Data + Valor'][~df_planilha['Chave Cartão + Data + Valor'].duplicated(keep=False)]
+    #chaves_validas_k3 = set(unicos_sql_k3).intersection(set(unicos_plan_k3))
 
-    # Chave KEY 5 🗝️
+    # Chave KEY 4 🗝️
     unicos_sql_k4 = df_sql['Chave Cartão + Data + Valor + Loc Cia'][~df_sql['Chave Cartão + Data + Valor + Loc Cia'].duplicated(keep=False)]
     unicos_plan_k4 = df_planilha['Chave Cartão + Data + Valor + Loc Cia'][~df_planilha['Chave Cartão + Data + Valor + Loc Cia'].duplicated(keep=False)]
     chaves_validas_k4 = set(unicos_sql_k4).intersection(set(unicos_plan_k4))
 
-    # Chave KEY 6 🗝️
+    # Chave KEY 5 🗝️
     unicos_sql_k5 = df_sql['Chave Cartão + Valor + Loc Cia'][~df_sql['Chave Cartão + Valor + Loc Cia'].duplicated(keep=False)]
     unicos_plan_k5 = df_planilha['Chave Cartão + Valor + Loc Cia'][~df_planilha['Chave Cartão + Valor + Loc Cia'].duplicated(keep=False)]
     chaves_validas_k5 = set(unicos_sql_k5).intersection(set(unicos_plan_k5))
@@ -44,7 +44,7 @@ def executar_matching(df_planilha, df_sql, df_depara):
 
     map_k1 = df_sql[df_sql['Chave Aut + Data + Valor'].isin(chaves_validas_k1)].set_index('Chave Aut + Data + Valor')
     map_k2 = df_sql[df_sql['Chave Loc Cia + Data + Valor'].isin(chaves_validas_k2)].set_index('Chave Loc Cia + Data + Valor')
-    map_k3 = df_sql[df_sql['Chave Cartão + Data + Valor'].isin(chaves_validas_k3)].set_index('Chave Cartão + Data + Valor')
+    #map_k3 = df_sql[df_sql['Chave Cartão + Data + Valor'].isin(chaves_validas_k3)].set_index('Chave Cartão + Data + Valor')
     map_k4 = df_sql[df_sql['Chave Cartão + Data + Valor + Loc Cia'].isin(chaves_validas_k4)].set_index('Chave Cartão + Data + Valor + Loc Cia')
     map_k5 = df_sql[df_sql['Chave Cartão + Valor + Loc Cia'].isin(chaves_validas_k5)].set_index('Chave Cartão + Valor + Loc Cia')
     #map_k7 = df_sql[df_sql['Chave Cartão + Valor'].isin(chaves_validas_k7)].set_index('Chave Cartão + Valor')
@@ -111,11 +111,13 @@ def executar_matching(df_planilha, df_sql, df_depara):
     chaves = [
         ('Chave Aut + Data + Valor', map_k1, chaves_validas_k1),
         ('Chave Loc Cia + Data + Valor', map_k2, chaves_validas_k2),
-        ('Chave Cartão + Data + Valor', map_k3, chaves_validas_k3),
+        #('Chave Cartão + Data + Valor', map_k3, chaves_validas_k3),
         ('Chave Cartão + Data + Valor + Loc Cia', map_k4, chaves_validas_k4),
         ('Chave Cartão + Valor + Loc Cia', map_k5, chaves_validas_k5),
         #('Chave Cartão + Valor', map_k7, chaves_validas_k7),
     ]
+
+    log_chaves = []
 
     # =========================
     # MATCH - FAZ LOOPING VALIDANDO ONDE HOUVE MATCH COM OS MAPAS E PREENCHE AS COLUNAS
@@ -128,8 +130,53 @@ def executar_matching(df_planilha, df_sql, df_depara):
 
         # só tenta quem ainda não encontrou nada
         mask = df_planilha['teve_match'] == False
-
         idx = df_planilha.loc[mask].index
+
+        for idx_linha in idx:
+
+            mask_original_linha = mask_original_columns.loc[idx_linha]
+            
+            chave_valor = df_planilha.at[idx_linha, nome_chave]
+
+            encontrou = chave_valor in chaves_validas
+
+            if not encontrou:
+                log_chaves.append({
+                    'idx': idx_linha,
+                    'chave': nome_chave,
+                    'resultado': 'nao_encontrado'
+                    })
+                continue
+
+            cliente = df_planilha.at[idx_linha, 'Nome da Empresa']
+            regra_final = regras_finais(cliente, regras_padrao, regras_clientes)
+            
+            linha_temp = df_planilha.loc[idx_linha].copy()
+
+            for col_dest, col_origem in regra_final.items():
+                if col_origem in mapa.columns:
+                    try:
+                        linha_temp[col_dest] = mapa.at[chave_valor, col_origem]
+                    except KeyError:
+                        continue
+
+            for col in regra_final.keys():
+                if linha_temp[col] == '' and mask_original_linha[col]:
+                    linha_temp[col] = '**********'
+
+            tem_ausencia = any(
+                linha_temp[col] == '**********'
+                for col in regra_final.keys()
+            )
+
+            resultado = 'incompleto' if tem_ausencia else 'Ok'
+
+            log_chaves.append({
+                'idx': idx_linha,
+                'chave': nome_chave,
+                'resultado': resultado 
+            })
+
         chaves_linha = df_planilha.loc[mask, nome_chave]
 
         idx_match = idx[chaves_linha.isin(chaves_validas)]
@@ -220,4 +267,4 @@ def executar_matching(df_planilha, df_sql, df_depara):
 
     df_preenchido = df_planilha[df_planilha['status'] == 'Ok'].copy()
 
-    return df_preenchido, df_nao_localizados
+    return df_preenchido, df_nao_localizados, pd.DataFrame(log_chaves)
